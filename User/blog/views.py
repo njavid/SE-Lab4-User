5b1b678175d57fec48b5e4d9366ed6990f58d9a7
narges <jatages@gmail.com>
2021-04-16 15:30:59 +0430

    Create user project

---
+++ User/blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def register(request):
+    return HttpResponse("register ")
+
+def login(request):
+    return HttpResponse("login ")
