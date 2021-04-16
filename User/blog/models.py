b2a6948bbbaae0e43f57a4c85f03440256fe3ad2
narges <jatages@gmail.com>
2021-04-16 16:17:06 +0430

    Create User database and migrate

--- User/blog/models.py
+++ User/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """A model of a rock band."""
+    username = models.CharField(max_length=50,primary_key=True)
+    password = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    mobile = models.CharField(max_length=11)
+    is_admin = models.BooleanField(default=False)
